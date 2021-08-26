1ceeac0b743314701b3f5214f13f148ffd42ec71
b1809128 <nghoquochuy0902@gmail.com>
2021-08-26 12:52:42 +0700

    api owner

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,81 +1,116 @@
-// https://documenter.getpostman.com/view/10808728/SzS8rjbc#27454960-ea1c-4b91-a0b6-0468bb4e6712
-// https://corona.lmao.ninja/v2/countries/vn
-const url = "https://corona.lmao.ninja/v2/countries/vn";
+const url = "https://covid-19-hoc-tap.herokuapp.com/api/getInfected.php";
 
 fetch(url)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data);
-        // *Data Total
-        var num_format_infected = new Intl.NumberFormat().format(data.cases)
-        var num_format_recovered = new Intl.NumberFormat().format(data.recovered)
-        var num_format_death = new Intl.NumberFormat().format(data.deaths)
-        var num_format_treat = new Intl.NumberFormat().format(data.active)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    getData(data);
+  })
+  .catch((error) => {
+    return error;
+  });
 
-        // *Data New
-        var numNewInfected = new Intl.NumberFormat().format(data.todayCases)
-        var numNewRecovered = new Intl.NumberFormat().format(data.todayRecovered)
-        var numNewDeath = new Intl.NumberFormat().format(data.todayDeaths)
-        var numNewTreat = new Intl.NumberFormat().format(data.todayCases)
+function getData(data) {
+  getLastUpdate();
+  getInfected(data.api.infected);
+  getDataToday(data.api.today);
+  getCircleData(data.api.circle);
+  getChart(data.api.date, data.api.sevenday, data.api.recovered);
+}
 
-        if (numNewInfected == 0) {
-            numNewInfected = "Loading..."
-        } else {
-            numNewInfected = new Intl.NumberFormat().format(data.todayCases)
-        }
+function getLastUpdate() {
+  const d = new Date();
+  document.getElementById("time").innerHTML = d;
+}
 
-        arr_update = [num_format_infected, num_format_recovered, num_format_death, num_format_treat];
-        // *Infected, Recovered, Death
-        for (let i = 0; i < document.querySelectorAll('.card__number').length; i++) {
-            document.querySelectorAll('.card__number')[i].innerHTML = arr_update[i];
-        }
+function getInfected(data) {
+  for (let i = 0; i < data.length; i++) {
+    document.querySelectorAll(".card-number")[i].innerHTML = data[i];
+  }
+}
 
-        // *Date Last Update
-        var get_Date = new Date(data.updated);
-        var string_date = get_Date.getDate() + "/" + (get_Date.getMonth() + 1) + "/" + get_Date.getFullYear();
-        var get_Time = new Date();
-        var string_time = get_Time.getHours() + ":" + get_Time.getMinutes() + ":" + get_Time.getSeconds();
+function getDataToday(data) {
+  for (let i = 0; i < data.length; i++) {
+    document.querySelectorAll(".card-number-today")[i].innerHTML = data[i];
+  }
+}
 
-        for (let i = 0; i < document.querySelectorAll('.card__date').length; i++) {
-            document.querySelectorAll('.card__date')[i].innerHTML = "Last Update: " + string_date + "-" + string_time + " GMT+7:00";
-        }
+// *Chart
+function getCircleData(data) {
+  var xValues = ["Số ca nhiễm", "Hồi phục", "Đang điều trị", "Tử vong"];
+  var yValues = [];
+  for (let i = 0; i < data.length; i++) {
+    yValues.push(data[i]);
+  }
+  var barColors = ["#b91d47", "#1e7145", "#2b5797", "rgba(0,0,0,0.8)"];
 
-        var arr_new = [numNewInfected, numNewRecovered, numNewDeath, numNewTreat];
+  new Chart("chart-circle", {
+    type: "doughnut",
+    data: {
+      labels: xValues,
+      datasets: [
+        {
+          backgroundColor: barColors,
+          data: yValues,
+        },
+      ],
+    },
+    options: {
+      title: {
+        display: true,
+      },
+    },
+  });
+}
 
-        //* Number Active
-        for (let i = 0; i < document.querySelectorAll('.card__number-active').length; i++) {
-            document.querySelectorAll('.card__number-active')[i].innerHTML = "+ " + arr_new[i];
-        }
+function getChart(x, y, z) {
+  var xValues = [];
+  var yValues = [];
+  var zValues = [];
 
+  for (let i = 0; i < x.length; i++) {
+    xValues.push(x[i]);
+    yValues.push(y[i] * 1000);
+    zValues.push(z[i]);
+  }
+  console.log(xValues);
+  console.log(yValues);
+  console.log(zValues);
 
-        var xValues = ["Infected", "Recovered", "Death", "Treatment"];
-        var yValues = [data.cases, data.recovered, data.deaths, data.active];
-        var barColors = ["rgba( 255, 57, 43, 0.6)", "#28a745", "#1877f2", "rgba( 225, 132, 37, 1)"];
-
-        new Chart("myChart", {
-            type: "bar",
-            data: {
-                labels: xValues,
-                datasets: [{
-                    backgroundColor: barColors,
-                    data: yValues
-                }]
+  new Chart("chart-square", {
+    type: "line",
+    data: {
+      labels: xValues,
+      datasets: [
+        {
+          data: yValues,
+          borderColor: "red",
+          fill: true,
+          label: "Số ca nhiễm",
+        },
+        {
+          data: zValues,
+          borderColor: "green",
+          fill: true,
+          label: "Hồi phục",
+        },
+      ],
+    },
+    options: {
+      legend: {
+        display: true,
+      },
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              // min: 500,
+              // max: 200000
             },
-            options: {
-                legend: { display: false },
-                title: {
-                    display: true,
-                    text: "Quoc Huy Developer 2021"
-                }
-            }
-        });
-    })
-
-// jQuery(document).ready(function($) {
-//     $('.card__number').counterUp({
-//         delay: 10,
-//         time: 5000
-//     })
-// })
+          },
+        ],
+      },
+    },
+  });
+}
